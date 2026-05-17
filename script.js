bce7ff64fcd94912f298e5de2202e6b92a075b85
Manam Karthikeya <karthikeyamanam9@gmail.com>
2026-05-17 15:31:45 +0530

    Initial commit: AR Pizza Viewer

---
+++ script.js
@@ -0,0 +1,3 @@
+// The custom camera streaming has been removed. 
+// Google's <model-viewer> web component handles the camera access natively 
+// when the user clicks the "View on your table" AR button.
